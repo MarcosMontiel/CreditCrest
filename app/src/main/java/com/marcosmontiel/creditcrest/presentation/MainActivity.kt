bdb19aca954379fcc963ca2e51222b3ba47ef446
Marcos Montiel <marcos-gab14@hotmail.com>
2023-08-07 08:56:38 -0600

    Dagger Hilt navigation was implemented

--- app/src/main/java/com/marcosmontiel/creditcrest/presentation/MainActivity.kt
+++ app/src/main/java/com/marcosmontiel/creditcrest/presentation/MainActivity.kt
@@ -15,8 +15,11 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
         setContent {
             CreditCrestTheme {
                 // A surface container using the 'background' color from the theme
@@ -28,18 +31,12 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
     }
+
 }
 
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CreditCrestTheme {
-        Greeting("Android")
-    }
 }
