3bfd3914008f58dce26e59297c9d573575b4b24d
Marcos Montiel <marcos-gab14@hotmail.com>
2023-08-08 16:14:24 -0600

    DefaultText.kt was created

---
+++ app/src/main/java/com/marcosmontiel/creditcrest/presentation/component/DefaultText.kt
@@ -0,0 +1,34 @@
+package com.marcosmontiel.creditcrest.presentation.component
+
+import androidx.compose.material.LocalTextStyle
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
+
+@Composable
+fun DefaultText(
+    modifier: Modifier = Modifier,
+    fontStyle: FontStyle = FontStyle.Normal,
+    fontWeight: FontWeight = FontWeight.Normal,
+    fontSize: TextUnit = TextUnit.Unspecified,
+    style: TextStyle = LocalTextStyle.current,
+    color: Color = Color.Unspecified,
+    title: String,
+) {
+
+    Text(
+        text = title,
+        modifier = modifier,
+        color = color,
+        fontSize = fontSize,
+        fontStyle = fontStyle,
+        fontWeight = fontWeight,
+        style = style,
+    )
+
+}
--- app/src/main/java/com/marcosmontiel/creditcrest/presentation/screen/login/component/LoginContent.kt
+++ app/src/main/java/com/marcosmontiel/creditcrest/presentation/screen/login/component/LoginContent.kt
@@ -1,15 +1,19 @@
 package com.marcosmontiel.creditcrest.presentation.screen.login.component
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Text
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.marcosmontiel.creditcrest.R
 import com.marcosmontiel.creditcrest.presentation.component.DefaultCard
 import com.marcosmontiel.creditcrest.presentation.component.DefaultSolidBackground
+import com.marcosmontiel.creditcrest.presentation.component.DefaultText
 import com.marcosmontiel.creditcrest.presentation.screen.login.LoginViewModel
 import com.marcosmontiel.creditcrest.presentation.ui.theme.Blue800
 
@@ -37,10 +41,16 @@ fun LoginContent(
                     .fillMaxWidth()
                     .padding(horizontal = 16.dp, vertical = 32.dp),
                 verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
             ) {
 
-                Text(text = "Test")
+                DefaultText(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentWidth(Alignment.CenterHorizontally),
+                    fontWeight = FontWeight.Bold,
+                    title = stringResource(R.string.login_title),
+                    style = MaterialTheme.typography.h6,
+                )
 
             }
 
