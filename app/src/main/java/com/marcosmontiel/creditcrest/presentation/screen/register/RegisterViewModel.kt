5dccd9fd5ca1454fcc7c5ba71181b2df0131cd9c
Marcos Montiel <marcos-gab14@hotmail.com>
2023-08-10 17:36:14 -0600

    Register files were added

--- app/src/main/java/com/marcosmontiel/creditcrest/presentation/screen/register/RegisterViewModel.kt
+++ app/src/main/java/com/marcosmontiel/creditcrest/presentation/screen/register/RegisterViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
-@HiltViewModel
 class RegisterViewModel @Inject constructor() : ViewModel() {
 
 }
