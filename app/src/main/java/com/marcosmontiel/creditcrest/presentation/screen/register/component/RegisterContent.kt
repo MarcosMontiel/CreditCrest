078444b45cc88dda0551c62e94fae90499fd54fd
Marcos Montiel <marcos-gab14@hotmail.com>
2023-08-11 13:25:19 -0600

    Register files were modified

--- app/src/main/java/com/marcosmontiel/creditcrest/presentation/screen/register/component/RegisterContent.kt
+++ app/src/main/java/com/marcosmontiel/creditcrest/presentation/screen/register/component/RegisterContent.kt
@@ -1,11 +1,17 @@
 package com.marcosmontiel.creditcrest.presentation.screen.register.component
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.marcosmontiel.creditcrest.presentation.component.DefaultSolidBackground
 import com.marcosmontiel.creditcrest.presentation.screen.register.RegisterViewModel
+import com.marcosmontiel.creditcrest.presentation.ui.theme.Blue800
 
 @Composable
 fun RegisterContent(
@@ -14,5 +20,16 @@ fun RegisterContent(
     navController: NavHostController,
     paddingValues: PaddingValues,
 ) {
+
+    Box(modifier = modifier.padding(paddingValues)) {
+
+        DefaultSolidBackground(
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .fillMaxHeight(fraction = 0.45f),
+            color = Blue800,
+        )
+
+    }
 
 }
