45ad0a417fe8f201f74e2e6840c3754167d88173
Marcos Montiel <marcos-gab14@hotmail.com>
2023-10-10 17:45:28 -0600

    Bottom bar was modified

--- app/src/main/java/com/marcosmontiel/creditcrest/presentation/component/DefaultIcon.kt
+++ app/src/main/java/com/marcosmontiel/creditcrest/presentation/component/DefaultIcon.kt
@@ -1,16 +1,17 @@
 package com.marcosmontiel.creditcrest.presentation.component
 
 import androidx.compose.material.Icon
+import androidx.compose.material.LocalContentAlpha
+import androidx.compose.material.LocalContentColor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import com.marcosmontiel.creditcrest.presentation.ui.theme.Gray200
 
 @Composable
 fun DefaultIcon(
     modifier: Modifier = Modifier,
-    color: Color = Gray200,
+    color: Color = LocalContentColor.current.copy(alpha = LocalContentAlpha.current),
     icon: ImageVector,
     description: String,
 ) {
