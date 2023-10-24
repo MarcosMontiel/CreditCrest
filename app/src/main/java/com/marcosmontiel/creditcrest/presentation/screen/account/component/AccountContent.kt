303007fa2f81adeb02e8aceecc386b2018977133
Marcos Montiel <marcos-gab14@hotmail.com>
2023-10-24 16:19:50 -0600

    Account screen was added

---
+++ app/src/main/java/com/marcosmontiel/creditcrest/presentation/screen/account/component/AccountContent.kt
@@ -0,0 +1,17 @@
+package com.marcosmontiel.creditcrest.presentation.screen.account.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+
+@Composable
+fun AccountContent(modifier: Modifier, navController: NavHostController) {
+
+    Box(modifier = modifier.padding(16.dp)) {
+
+    }
+
+}
