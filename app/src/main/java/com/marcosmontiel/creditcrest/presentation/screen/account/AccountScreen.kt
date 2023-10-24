78f703621d5844ee898f2a6bc99211e4ea6c6de3
Marcos Montiel <marcos-gab14@hotmail.com>
2023-10-24 17:33:17 -0600

    TopAppBar was added into Account screen

--- app/src/main/java/com/marcosmontiel/creditcrest/presentation/screen/account/AccountScreen.kt
+++ app/src/main/java/com/marcosmontiel/creditcrest/presentation/screen/account/AccountScreen.kt
@@ -22,7 +22,13 @@ fun AccountScreen(navController: NavHostController) {
         scaffoldState = scaffoldState,
         topBar = {
 
-            DefaultTopBar(title = { DefaultText(title = stringResource(R.string.account_title)) })
+            DefaultTopBar(
+                title = {
+
+                    DefaultText(title = stringResource(R.string.account_title))
+
+                }
+            )
 
         },
         bottomBar = {},
