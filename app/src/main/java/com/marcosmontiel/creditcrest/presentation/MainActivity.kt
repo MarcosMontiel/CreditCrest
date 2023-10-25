8cf916cfe6ee3e3c22a151ac0b7b39e187abe07c
Marcos Montiel <marcos-gab14@hotmail.com>
2023-10-24 18:05:06 -0600

    Status bar color was changed

--- app/src/main/java/com/marcosmontiel/creditcrest/presentation/MainActivity.kt
+++ app/src/main/java/com/marcosmontiel/creditcrest/presentation/MainActivity.kt
@@ -3,13 +3,20 @@ package com.marcosmontiel.creditcrest.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.LocalAbsoluteElevation
+import androidx.compose.material.LocalElevationOverlay
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.marcosmontiel.creditcrest.presentation.navigation.RootNavGraph
+import com.marcosmontiel.creditcrest.presentation.ui.theme.Blue500
+import com.marcosmontiel.creditcrest.presentation.ui.theme.BlueGray900
 import com.marcosmontiel.creditcrest.presentation.ui.theme.CreditCrestTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -23,6 +30,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
+            val statusBarColor = if (isSystemInDarkTheme()) BlueGray900 else Blue500
+            window.statusBarColor = statusBarColor.toArgb()
+
             CreditCrestTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
