72aca2e6a659a831b04a5ea6d648ffe11bc9baa0
Marcos Montiel <marcos-gab14@hotmail.com>
2023-11-06 16:53:10 -0600

    Data classes were modified

--- app/src/main/java/com/marcosmontiel/creditcrest/presentation/screen/login/LoginState.kt
+++ app/src/main/java/com/marcosmontiel/creditcrest/presentation/screen/login/LoginState.kt
@@ -7,15 +7,18 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 
 data class LoginState(
+    // Email
     val email: String = "",
     val emailEnabled: Boolean = true,
     val emailEraser: Boolean = false,
-    val forgotPasswordButtonEnabled: Boolean = true,
-    val informationFillCorrect: Boolean = false,
-    val loginButtonEnabled: Boolean = true,
+    // Password
     val password: String = "",
     val passwordEnabled: Boolean = true,
     val passwordIcon: ImageVector = Icons.Rounded.Visibility,
     val passwordTransformation: VisualTransformation = PasswordVisualTransformation(),
+    // Validations
+    val forgotPasswordButtonEnabled: Boolean = true,
+    val informationFillCorrect: Boolean = false,
+    val loginButtonEnabled: Boolean = true,
     val signUpButtonEnabled: Boolean = true,
 )
--- app/src/main/java/com/marcosmontiel/creditcrest/presentation/screen/register/RegisterState.kt
+++ app/src/main/java/com/marcosmontiel/creditcrest/presentation/screen/register/RegisterState.kt
@@ -8,23 +8,28 @@ import androidx.compose.ui.text.input.VisualTransformation
 import com.marcosmontiel.creditcrest.presentation.enum.PasswordStrength
 
 data class RegisterState(
+    // Username
+    val username: String = "",
+    val usernameEnabled: Boolean = true,
+    val usernameEraser: Boolean = false,
+    // Email
     val email: String = "",
     val emailEnabled: Boolean = true,
     val emailEraser: Boolean = false,
-    val informationFillCorrect: Boolean = false,
-    val loginButtonEnabled: Boolean = true,
+    // Password
     val password: String = "",
     val passwordEnabled: Boolean = true,
     val passwordIcon: ImageVector = Icons.Rounded.Visibility,
     val passwordMatch: Boolean = true,
     val passwordStrength: PasswordStrength = PasswordStrength.EMPTY,
     val passwordTransformation: VisualTransformation = PasswordVisualTransformation(),
+    // Password confirmation
     val passwordConfirmation: String = "",
     val passwordConfirmationEnabled: Boolean = true,
     val passwordConfirmationIcon: ImageVector = Icons.Rounded.Visibility,
     val passwordConfirmationTransformation: VisualTransformation = PasswordVisualTransformation(),
+    // Validations
+    val informationFillCorrect: Boolean = false,
+    val loginButtonEnabled: Boolean = true,
     val signUpButtonEnabled: Boolean = true,
-    val username: String = "",
-    val usernameEnabled: Boolean = true,
-    val usernameEraser: Boolean = false,
 )
