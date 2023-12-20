90294cca769886433a1482914c65f3f76ee8bafa
Marcos Montiel <marcos-gab14@hotmail.com>
2023-12-20 16:34:49 -0600

    Progress indicator was modified

--- app/src/main/java/com/marcosmontiel/creditcrest/presentation/component/DefaultProgressIndicator.kt
+++ app/src/main/java/com/marcosmontiel/creditcrest/presentation/component/DefaultProgressIndicator.kt
@@ -1,31 +1,29 @@
 package com.marcosmontiel.creditcrest.presentation.component
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import com.marcosmontiel.creditcrest.presentation.ui.theme.BackgroundDarkColor
-import com.marcosmontiel.creditcrest.presentation.ui.theme.BackgroundLightColor
 
 @Composable
 fun DefaultProgressIndicator(
     modifier: Modifier = Modifier,
-    background: Color = if (isSystemInDarkTheme()) BackgroundDarkColor else BackgroundLightColor,
+    background: Color = MaterialTheme.colors.background
 ) {
 
     Box(
         modifier = modifier
             .fillMaxSize()
-            .background(
-                color = background.copy(alpha = 0.6f)
-            )
+            .background(color = background.copy(alpha = 0.6f))
     ) {
+
         CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+
     }
 
 }
